c7b44f6d6175a62cd6fc0c8715aed41a7e9279bf
gechengze <gechengze@163.com>
2022-08-26 14:20:43 +0800

    update NNLM

--- NNLM/NNLM.py
+++ NNLM/NNLM.py
@@ -1,55 +1,43 @@
 import torch
+import torchtext
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-import jieba
 import pandas as pd
 import re
 from tqdm.notebook import tqdm
+from collections import Counter
 
 
 # 构造数据集
 class MyDataset(Dataset):
-    def __init__(self, debug=False):
-        df = pd.read_csv('../../datasets/THUCNews/train.csv')
+    def __init__(self, file_path, tokenizer, stopwords, debug=True):
+        df = pd.read_csv(file_path)
         df = df.dropna().reset_index(drop=True)
         if debug:
             df = df.sample(2000).reset_index(drop=True)
         else:
             df = df.sample(50000).reset_index(drop=True)
         # 读取常用停用词
-        stopwords = [line.strip() for line in
-                     open('../../stopwords/cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
+        counter = Counter()
         sentences = []
         for title in tqdm(df['title']):
             # 去除标点符号
             title = re.sub(r'[^\u4e00-\u9fa5]', '', title)
-            # jieba分词
-            sentence_seged = jieba.cut(title.strip())
-            outstr = ''
-            for word in sentence_seged:
-                if word != '\t' and word not in stopwords:
-                    outstr += word
-                    outstr += ' '
-            if outstr != '':
-                sentences.append(outstr)
-                # 获取所有词（token）
-        token_list = list(set(' '.join(sentences).split()))
-        # token和index互转字典
-        self.token2idx = {token: i for i, token in enumerate(token_list)}
-        self.idx2token = {i: token for i, token in enumerate(token_list)}
-        self.vocab_size = len(self.token2idx)
+            tokens = [token for token in tokenizer(title.strip()) if token not in stopwords]
+            counter.update(tokens)
+            sentences.append(tokens)
+        self.vocab = torchtext.vocab.vocab(counter, specials=['<unk>', '<pad>'])
 
         # 构造输入和输出，输入是每三个词，输出是这三个词的下一个词，也就是简单的n-gram语言模型（n=3）
         self.inputs = []
         self.labels = []
         for sen in sentences:
-            sen = sen.split()
             for i in range(len(sen) - 3):
-                self.inputs.append([self.token2idx[token] for token in sen[i: i + 3]])
-                self.labels.append([self.token2idx[sen[i + 3]]])
+                self.inputs.append(self.vocab.lookup_indices(sen[i: i + 3]))
+                self.labels.append([self.vocab[sen[i + 3]]])
 
     def __len__(self):
         return len(self.labels)
@@ -57,6 +45,14 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         # 返回一个x和一个y
         return torch.LongTensor(self.inputs[idx]), torch.LongTensor(self.labels[idx])
+
+
+file_path = '../data/THUCNews/train.csv'
+debug = True
+tokenizer = torchtext.data.utils.get_tokenizer('spacy', language='zh_core_web_sm')
+stopwords = [line.strip() for line in open('../stopwords/cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
+dataset = MyDataset(file_path, tokenizer, stopwords)
+dataloader = DataLoader(dataset=dataset, batch_size=128, shuffle=True)
 
 
 class NNLM(nn.Module):
@@ -80,15 +76,13 @@ class NNLM(nn.Module):
         return y
 
 
-# 构造数据集
-dataset = MyDataset(debug=True)
-# 构造dataloader，batch size设置为128
-dataloader = DataLoader(dataset=dataset, batch_size=128, shuffle=True)
-
 # 初始化模型
-model = NNLM(vocab_size=len(dataset.token2idx), embed_size=256, n_step=3, n_hidden=256)
+model = NNLM(vocab_size=len(dataset.vocab), embed_size=256, n_step=3, n_hidden=256)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# 查看模型
+print(model)
 
 # 训练20个epoch
 for epoch in range(20):
@@ -100,12 +94,12 @@ for epoch in range(20):
         optimizer.step()
     print('epoch:', epoch + 1, 'loss =', '{:.6f}'.format(loss))
 
-
 # 使用训练好的模型进行预测，train_input直接是上面代码中的，直接用
 # 模型输出之后取argmax，再用idx2token转回单词，查看效果，可以看到效果还可以，有上下文关系
 predict = model(train_input).data.max(1, keepdim=True)[1].squeeze_().tolist()
 input_list = train_input.tolist()
 for i in range(len(input_list)):
-    print(dataset.idx2token[input_list[i][0]] + ' ' +
-          dataset.idx2token[input_list[i][1]] + ' ' +
-          dataset.idx2token[input_list[i][2]] + ' -> ' + dataset.idx2token[predict[i]])
+    print(dataset.vocab.get_itos()[input_list[i][0]] + ' ' +
+          dataset.vocab.get_itos()[input_list[i][1]] + ' ' +
+          dataset.vocab.get_itos()[input_list[i][2]] + ' -> ' +
+          dataset.vocab.get_itos()[predict[i]])
