a2ed187ed291fecb8b9e78233dd6c977f17a4f9d
gechengze <gechengze@163.com>
2022-08-29 15:01:45 +0800

    update TextCNN

--- TextCNN/TextCNN.py
+++ TextCNN/TextCNN.py
@@ -14,6 +14,8 @@ import re
 from collections import Counter
 from tqdm.notebook import tqdm
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 能用gpu则用gpu
+
 
 class MyDataset(Dataset):
     def __init__(self, file_path, tokenizer, stopwords, debug=True):
@@ -21,8 +23,6 @@ class MyDataset(Dataset):
         df = df.dropna().reset_index(drop=True)
         if debug:
             df = df.sample(2000).reset_index(drop=True)
-        else:
-            df = df.sample(50000).reset_index(drop=True)
         counter = Counter()
         sentences = []
         for title in tqdm(df['title']):
@@ -32,6 +32,7 @@ class MyDataset(Dataset):
             counter.update(tokens)
             sentences.append(tokens)
         self.vocab = torchtext.vocab.vocab(counter, specials=['<unk>', '<pad>'])
+        self.vocab.set_default_index(self.vocab['<unk>'])
 
         self.inputs = [self.vocab.lookup_indices(tokens) for tokens in sentences]
         self.labels = [[label] for label in df['label'].values.tolist()]
@@ -47,7 +48,7 @@ class MyDataset(Dataset):
 file_path = '../data/THUCNews/train.csv'
 tokenizer = torchtext.data.utils.get_tokenizer('spacy', language='zh_core_web_sm')
 stopwords = [line.strip() for line in open('../stopwords/cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
-dataset = MyDataset(file_path, tokenizer, stopwords)
+dataset = MyDataset(file_path, tokenizer, stopwords, debug=False)
 
 
 def collate_fn(batch_data):
@@ -55,7 +56,7 @@ def collate_fn(batch_data):
         [y for x, y in batch_data]).unsqueeze(1)
 
 
-dataloader = DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
+dataloader = DataLoader(dataset, batch_size=1024, shuffle=True, collate_fn=collate_fn)
 
 
 class TextCNN(nn.Module):
@@ -83,16 +84,32 @@ class TextCNN(nn.Module):
         return logits
 
 
-model = TextCNN(vocab_size=len(dataset.vocab), embed_size=128, n_class=dataset.n_class)
+model = TextCNN(vocab_size=len(dataset.vocab), embed_size=256, n_class=dataset.n_class).to(device)
 print(model)
-
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
-for epoch in range(100):
+model.train()
+for epoch in range(20):
     for feature, target in dataloader:
+        feature = feature.to(device)
+        target = target.to(device)
         optimizer.zero_grad()
         logits = model(feature)
         loss = criterion(logits, target.squeeze())
         loss.backward()
         optimizer.step()
     print('epoch:', epoch + 1, ', loss:', loss.item())
+
+model.eval()
+df_train = pd.read_csv('../data/THUCNews/train.csv')
+df_test = pd.read_csv('../data/THUCNews/test.csv')
+for i, row in df_test.sample(10).iterrows():
+    title = row['title']
+    actual = row['class']
+    title = re.sub(r'[^\u4e00-\u9fa5]', '', title)
+    tokens = [token for token in tokenizer(title.strip()) if token not in stopwords]
+    inputs = dataset.vocab.lookup_indices(tokens)
+    inputs = torch.LongTensor(inputs).unsqueeze(1).to(device)
+    predict = model(inputs)
+    predict_class = dict(zip(df_train['label'], df_train['class']))[predict.max(1)[1].item()]
+    print(' '.join(tokens), '||| actual:', actual, ', predict:', predict_class)
