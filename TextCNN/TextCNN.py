5549e43fc5b3afdcdc47cc1e67edd5f8d9a28099
gechengze <gechengze@163.com>
2022-08-29 14:25:09 +0800

    update TextCNN

--- TextCNN/TextCNN.py
+++ TextCNN/TextCNN.py
@@ -1,53 +1,39 @@
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
 import torch
+import torchtext
 import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 import pandas as pd
-import jieba
 import re
+from collections import Counter
+from tqdm.notebook import tqdm
 
 
 class MyDataset(Dataset):
-    def __init__(self, max_len, debug=True):
-        super().__init__()
-        df = pd.read_csv('../../datasets/THUCNews/train.csv')
-        df = df.dropna()
+    def __init__(self, file_path, tokenizer, stopwords, debug=True):
+        df = pd.read_csv(file_path)
+        df = df.dropna().reset_index(drop=True)
         if debug:
             df = df.sample(2000).reset_index(drop=True)
         else:
             df = df.sample(50000).reset_index(drop=True)
-        # 读取常用停用词
-        stopwords = [line.strip() for line in open('../../stopwords/cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
+        counter = Counter()
         sentences = []
-        for title in df['title']:
+        for title in tqdm(df['title']):
             # 去除标点符号
             title = re.sub(r'[^\u4e00-\u9fa5]', '', title)
-            # jieba分词
-            sentence_seged = jieba.cut(title.strip())
-            outstr = ''
-            for word in sentence_seged:
-                if word != '\t' and word not in stopwords:
-                    outstr += word
-                    outstr += ' '
-            if outstr != '':
-                sentences.append(outstr)
-        # 获取所有词(token), <pad>用来填充不满足max_len的句子
-        token_list = ['<pad>'] + list(set(' '.join(sentences).split()))
-        # token和index互转字典
-        self.token2idx = {token: i for i, token in enumerate(token_list)}
-        self.idx2token = {i: token for i, token in enumerate(token_list)}
-        self.vocab_size = len(self.token2idx)
+            tokens = [token for token in tokenizer(title.strip()) if token not in stopwords]
+            counter.update(tokens)
+            sentences.append(tokens)
+        self.vocab = torchtext.vocab.vocab(counter, specials=['<unk>', '<pad>'])
 
-        self.inputs = []
-        for sentence in sentences:
-            tokens = sentence.split()
-            input_ = [self.token2idx[token] for token in tokens]
-            if len(input_) < max_len:
-                self.inputs.append(input_ + [self.token2idx['<pad>']] * (max_len - len(input_)))
-            else:
-                self.inputs.append(input_[: max_len])
-
+        self.inputs = [self.vocab.lookup_indices(tokens) for tokens in sentences]
         self.labels = [[label] for label in df['label'].values.tolist()]
         self.n_class = len(df['label'].unique())
 
@@ -56,6 +42,20 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         return torch.LongTensor(self.inputs[idx]), torch.LongTensor(self.labels[idx])
+
+
+file_path = '../data/THUCNews/train.csv'
+tokenizer = torchtext.data.utils.get_tokenizer('spacy', language='zh_core_web_sm')
+stopwords = [line.strip() for line in open('../stopwords/cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
+dataset = MyDataset(file_path, tokenizer, stopwords)
+
+
+def collate_fn(batch_data):
+    return pad_sequence([x for x, y in batch_data], padding_value=1), torch.tensor(
+        [y for x, y in batch_data]).unsqueeze(1)
+
+
+dataloader = DataLoader(dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
 
 
 class TextCNN(nn.Module):
@@ -70,6 +70,7 @@ class TextCNN(nn.Module):
         self.fc = nn.Linear(16, n_class)
 
     def forward(self, x):  # x: [batch_size * 句子长度]
+        x = x.permute(1, 0)
         x = self.embedding(x)  # [batch_size * 句子长度 * embed_size]
         x = x.unsqueeze(1)  # [batch_size * 1 * 句子长度 * embed_size]，加一个维度，用于卷积层的输入
         x = self.conv(x)  # [batch_size * 16(卷积层输出通道数) * 8(卷积后的宽) * 1(卷积后的高)]
@@ -82,9 +83,9 @@ class TextCNN(nn.Module):
         return logits
 
 
-dataset = MyDataset(max_len=10)  # 构造长度为10的句子输入，超过10的句子切掉，不足10的补<pad>
-dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
-model = TextCNN(vocab_size=dataset.vocab_size, embed_size=128, n_class=dataset.n_class)
+model = TextCNN(vocab_size=len(dataset.vocab), embed_size=128, n_class=dataset.n_class)
+print(model)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
 for epoch in range(100):
@@ -95,11 +96,3 @@ for epoch in range(100):
         loss.backward()
         optimizer.step()
     print('epoch:', epoch + 1, ', loss:', loss.item())
-
-
-df = pd.read_csv('../../datasets/THUCNews/train.csv')
-predict = model(feature).max(1)[1].tolist()
-for i in range(len(feature.tolist())):
-    print(' '.join([dataset.idx2token[idx] for idx in feature.tolist()[i]]),
-          '---> label:',
-          dict(zip(df['label'], df['class']))[predict[i]])
